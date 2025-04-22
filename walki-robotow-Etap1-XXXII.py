658663c3d07fd0a95bd6893e989a4e822df894bb
MainFoldFunc <gs.live.pub@gmail.com>
2025-04-22 11:54:23 +0200

    Can't do this task

---
+++ walki-robotow-Etap1-XXXII.py
@@ -0,0 +1,18 @@
+def roz1():
+    n = int(input())
+    hash_map = {}
+    for i in range(n):
+        s, z = input().split()
+        sint = int(s)
+        zint = int(z)
+        hash_map[sint] = zint
+
+    used = set()
+    for (i, j) in zip(hash_map):
+        if 
+
+def main():
+    roz1()
+
+
+main()
