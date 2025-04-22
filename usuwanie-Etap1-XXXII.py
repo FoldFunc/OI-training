3ff1c677c2d7c2db487b69bb053c4918fc537fc6
MainFoldFunc <gs.live.pub@gmail.com>
2025-04-22 11:13:45 +0200

    second answer max points

--- usuwanie-Etap1-XXXII.py
+++ usuwanie-Etap1-XXXII.py
@@ -1,29 +1,43 @@
 def roz1():
     fin_score = 0
-    a, b = input("").split()
+    a, b = input().split()
     aint = int(a)
     bint = int(b)
 
-    pula = []
-    for i in range(aint, bint + 1):
-        pula.append(i)
-
+    pula = list(range(aint, bint + 1))
+    used = set()
 
     for i in range(len(pula)):
-        for j in range(len(pula)):
-            if (pula[i] + pula[j]) % 2 == 0 and i != j and pula[i] != 0 and pula[j] != 0:
-                pula[i] = 0
-                pula[j] = 0
+        if i in used:
+            continue
+        for j in range(i + 1, len(pula)):
+            if j in used:
+                continue
+            if (pula[i] + pula[j]) % 2 == 0:
+                used.add(i)
+                used.add(j)
                 fin_score += 2
-
 
     print(fin_score)
 def roz2():
-    pass
-
+    fin_score = 0
+    a, b = input().split()
+    aint = int(a)
+    bint = int(b)
+    even_count = (bint // 2) - ((aint - 1) // 2) 
+    odd_count = (bint - aint + 1) - even_count
+    if even_count % 2 == 0 and odd_count % 2 == 0:
+        fin_score += odd_count + even_count
+    elif even_count % 2 == 0 and odd_count % 2 != 0:
+        fin_score += even_count + odd_count - 1
+    elif even_count % 2 != 0 and odd_count % 2 == 0:
+        fin_score += even_count - 1 + odd_count
+    elif even_count % 2 != 0 and odd_count %2 != 0:
+        fin_score += even_count -1 + odd_count - 1
+    print(fin_score)
 
 def main():
-    roz1()
+    roz2()
 
 
 main()
